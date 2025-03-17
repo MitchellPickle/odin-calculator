f2ecbfc770287208c2785f8eb47efc6c430d881c
MitchellPickle <94079796+MitchellPickle@users.noreply.github.com>
2025-03-17 16:26:53 +0000

    Updated README.md, added comments to scripts.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # odin-calculator
 The Odin Project's Calculator project.
+This project utilizes ChatGPT for the functions (as requested by the instructor). This is a simple calculator that isn't complex, but quick and concise.
--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+//IMPORTANT NOTE: I utilized ChatGPT to build the calculator functions for this app (as requested by the instructor)
+/*ChatGPT Approximate Prompt (My computer crashed and I don't remember the exact prompt from ChatGPT).
+Prompt: "calculator functions with html buttons and javascript without using eval and two numbers at a time"
+*/
+
 let firstNumber = '';
 let secondNumber = '';
 let currentOperator = null;
+
+//numberCheck is used to make sure firstNum is defined.
 let numberCheck = false;
 
+//Function is used to "update" the text area, used at the end of other functions.
 function updateDisplay()
 {
     let displayValue = firstNumber;
@@ -11,9 +19,11 @@ function updateDisplay()
         displayValue += ` ${currentOperator} ${secondNumber}`;
     }
 
+    //Display the single/combined value in the text area.
     document.getElementById('display-area').value = displayValue;
 }
 
+//Clears all number variables, operator variable, and sets the check to false.
 function clearFunction()
 {
     firstNumber = '';
@@ -23,15 +33,19 @@ function clearFunction()
     updateDisplay();
 }
 
+//Stores the button input for both number variables.
 function numberInput(number)
 {
     if(numberCheck == true)
     {
         secondNumber = number;
+
+        //numberCheck is set to false since the secondNumber was recorded.
         numberCheck = false;
     }
     else
     {
+        //Keep adding digits to the first number if there isn't an operator
         if(currentOperator === null)
         {
             firstNumber += number;
@@ -44,8 +58,10 @@ function numberInput(number)
     updateDisplay();
 }
 
+//Function is used when an operator button is clicked, setting the operator.
 function setOperator(operator)
 {
+    //Function calls calculateResult() if there's an operator and two numbers.
     if(currentOperator !== null && secondNumber !== '')
     {
         calculateResult();
@@ -55,12 +71,14 @@ function setOperator(operator)
     numberCheck = true;
 }
 
+//Function calculates two given numbers with a chosen operator.
 function calculateResult()
 {
     let numOne = parseFloat(firstNumber);
     let numTwo = parseFloat(secondNumber);
     let result;
 
+    //Returns an error is either number is "Not a Number".
     if(isNaN(numOne) || isNaN(numTwo))
     {
         result = 'ERR';
@@ -69,16 +87,21 @@ function calculateResult()
     {
         switch(currentOperator)
         {
+            //Addition Case
             case '+':
                 result = numOne + numTwo;
                 break;
+            //Subtraction Case 
             case '-':
                 result = numOne - numTwo;
                 break;
+            //Multiplication Case
             case '*':
                 result = numOne * numTwo;
                 break;
+            //Division Case
             case '/':
+                //Returns an error if dividing by zero.
                 result = numTwo === 0 ? 'ERR' : numOne / numTwo;
                 break;
         }
